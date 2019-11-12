24046744d2dedc50cf86af7760db845ec485ed1a
Guillaume Bournique <guillaume.bournique@eigentech.com>
2019-11-12 11:39:51 +0000

    set up ansible files and parameters

--- github-deployment/start_deployment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-VAULT="github-deployment/roles/setup/vars/ansible-vault-pw"
-INVENTORY_FILE="inventory.yml"
-PLAYBOOK="site.yml"
-
-ansible-playbook -i $INVENTORY_FILE --vault-id $VAULT $PLAYBOOK
